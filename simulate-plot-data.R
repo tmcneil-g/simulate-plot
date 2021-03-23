1a7ca8ebb88573a3681cb8a4df0858f1e0423307
Torbet McNeil <tmcneil@email.arizona.edu>
2021-03-23 10:29:03 -0700

    Increase noise in y value

--- simulate-plot-data.R
+++ simulate-plot-data.R
@@ -5,7 +5,7 @@
 
 # Simulate data from normal distribution
 X <- rnorm(n= 100)
-y <- 2 * X + rnorm(n = 100, sd =0.1)
+y <- 2 * X + rnorm(n = 100, sd =0.2)
 
 # Plot simulated data
 plot(x = X, y = y)
