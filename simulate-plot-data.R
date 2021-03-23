569db4f00675d20d787ba8c42f58048fa24c0bf6
tmcneil-g <56936008+tmcneil-g@users.noreply.github.com>
2021-03-23 10:46:43 -0700

    Increase noise of y paramater

--- simulate-plot-data.R
+++ simulate-plot-data.R
@@ -5,7 +5,7 @@
 
 # Simulate data from normal distribution
 X <- rnorm(n= 100)
-y <- 3 * X + rnorm(n = 100, sd =0.5)
+y <- 3 * X + rnorm(n = 100, sd =0.50)
 
 # Plot simulated data
 plot(x = X, y = y, main = "Simulated Data")
