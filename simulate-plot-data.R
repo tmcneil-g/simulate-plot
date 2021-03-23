90f6d6d09a14002ffb6428934ea9dda20b28cd4e
Torbet McNeil <tmcneil@email.arizona.edu>
2021-03-23 10:07:40 -0700

    Initial commit with data simulation and plot

---
+++ simulate-plot-data.R
@@ -0,0 +1,11 @@
+# Simulate and plot data
+# Torbet McNeil 
+# tmcneil@email.arizona.edu
+# 2021-03-23
+
+# Simulate data from normal distribution
+X <- rnorm(n= 100)
+y <- 2 * X + rnorm(n = 100, sd =0.1)
+
+# Plot simulated data
+plot(x = X, y = y)
